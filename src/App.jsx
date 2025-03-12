de813aac2fa65663d8157a6d638a9176a2496515
Alex Melander <Alexandermelander1@gmail.com>
2025-03-12 16:25:07 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section className="bg-gray-800 h-screen">
+        <h1 className="text-3xl text-white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, cum.
+        </h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
