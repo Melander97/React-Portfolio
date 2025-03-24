c513674ca03b62c6b910b2358c7ecdc3e1b318e9
Alex Melander <Alexandermelander1@gmail.com>
2025-03-24 10:14:20 +0100

    feat: add hamburger menu mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,69 @@
+import react, { useState } from "react";
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="p-4 bg-gray-900 ">
-      <div className="flex justify-between items-center max-w-[1200px] mx-auto">
+    <nav className="bg-gray-700 p-4">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold">My Portfolio</div>
-        <ul className="flex space-x-8 text-white ">
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-white focus:outline-none"
+          >
+            {/* Hamburger Icon */}
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <ul
+          className={`${
+            isOpen ? "block" : "hidden"
+          } md:flex md:space-x-4 mt-4 md:mt-0`}
+        >
           <li>
-            <a href="#">Home</a>
+            <a
+              href="#home"
+              className="text-white hover:text-gray-400 block md:inline"
+            >
+              Home
+            </a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a
+              href="#about"
+              className="text-white hover:text-gray-400 block md:inline"
+            >
+              About
+            </a>
           </li>
           <li>
-            <a href="#">Projects</a>
+            <a
+              href="#projects"
+              className="text-white hover:text-gray-400 block md:inline"
+            >
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a
+              href="#contact"
+              className="text-white hover:text-gray-400 block md:inline"
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </div>
