ae60eeeba083068f5d544b56f8f89b46905d9517
Alexander Melander <alex.melander@kreng.se>
2025-04-05 21:30:02 +0200

    feat:create project component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Portrait from "./assets/images/portrait.jpg";
 import Tech from "./components/Tech";
+import Projects from "./components/Projects";
 
 function App() {
   const heroData = {
@@ -28,6 +29,11 @@ function App() {
         {/*tech stack section */}
         <section id="tech">
           <Tech />
+        </section>
+
+        {/* projects section */}
+        <section id="projects" className="min-h-[80vh] bg-gray-900">
+          <Projects />
         </section>
       </main>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,64 @@
+import ProjectCard from "./ProjectCard";
+
 export default function Projects() {
+  const projectsData = [
+    {
+      title: "Kreng",
+      description: "Description for project 1",
+      image: "https://placehold.co/150  ",
+      technologies: [
+        "React",
+        "JavaScript",
+        "CSS",
+        "HTML",
+        "Node.js",
+        "Express",
+      ],
+      link: "https://kreng.se/",
+    },
+    {
+      title: "Trolska skogen",
+      description: "Description for project 2",
+      image: "https://placehold.co/400",
+      technologies: ["HTML", "CSS"],
+      link: "https://www.trolskaskogen.se/",
+    },
+    {
+      title: "Thermo Nord",
+      description: "Description for project 3",
+      image: "https://placehold.co/200",
+      technologies: ["JavaScript", "CSS"],
+      link: "https://thermonord.se/",
+    },
+    {
+      title: "Urban Advisors",
+      description: "Description for project 4",
+      image: "https://placehold.co/800",
+      technologies: ["React", "JavaScript"],
+      link: "https://urbanadvisors.se/",
+    },
+    {
+      title: "Teamtech",
+      description: "Description for project 4",
+      image: "https://placehold.co/800",
+      technologies: ["React", "JavaScript"],
+      link: "https://teamtech.se/",
+    },
+  ];
+
   return (
-    <>
-      <h1 className="text-red-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-        distinctio explicabo iure adipisci veritatis quia voluptatem error
-        repellendus ipsum dolore.
+    <section
+      id="projects"
+      className="flex min-h-screen w-full flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24"
+    >
+      <h1 className="text-4xl md:text-6xl text-white font-light">
+        My Projects
       </h1>
-    </>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 w-full max-w-[1200px]">
+        {projectsData.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+    </section>
   );
 }
