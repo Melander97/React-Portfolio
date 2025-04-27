815f2524c7b9156ba18e2973bc53cf0823c7e413
Alexander Melander <alex.melander@kreng.se>
2025-04-27 10:55:01 +0200

    fix(vite.config.js): optimized file for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import viteCompression from "vite-plugin-compression";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteCompression({
-      algorithm: "brotli",
-      threshold: 1024,
-      ext: ".br",
-      filter: /\.(js|css|json|html|svg)$/i,
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    target: "esnext",
+    assetsInlineLimit: 4096,
+    sourcemap: false,
+    cssCodeSplit: true,
+  },
 });
